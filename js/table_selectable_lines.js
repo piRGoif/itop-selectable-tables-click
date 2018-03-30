e62732bcbb484d19374fe876edcf7664727987bb
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-03-30 17:30:53 +0200

    Launch JS on ready

--- js/table_selectable_lines.js
+++ js/table_selectable_lines.js
@@ -1,18 +1,20 @@
-//TODO blue background color for selected lines
+$(document).ready(function() {
+	//TODO blue background color for selected lines
 
-var $tablesWithSelectableLines = $("table").find("tbody>tr>td>input:checkbox").closest("table");
-//FIXME error if none found
-$tablesWithSelectableLines.off(); //FIXME only here for scratchpad use
-$tablesWithSelectableLines.css("cursor", "pointer");
-$tablesWithSelectableLines.on("click", "tbody>tr", function(event) {
-  var $eventTarget = $(event.target);
-  if ($eventTarget.is("a")) {
-    return;
-  }
-  if ($eventTarget.is("button")) {
-    return;
-  }
-  
-  var $lineCheckbox = $eventTarget.closest("tr").find("td>input:checkbox");
-  $lineCheckbox.click();
+	var $tablesWithSelectableLines = $("table").find("tbody>tr>td>input:checkbox").closest("table");
+	//FIXME error if none found
+	$tablesWithSelectableLines.off(); //FIXME only here for scratchpad use
+	$tablesWithSelectableLines.css("cursor", "pointer");
+	$tablesWithSelectableLines.on("click", "tbody>tr", function (event) {
+		var $eventTarget = $(event.target);
+		if ($eventTarget.is("a")) {
+			return;
+		}
+		if ($eventTarget.is("button")) {
+			return;
+		}
+
+		var $lineCheckbox = $eventTarget.closest("tr").find("td>input:checkbox");
+		$lineCheckbox.click();
+	});
 });
